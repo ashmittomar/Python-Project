7c2bec59bfcf81b2f845174d098503f6ff5bee99
Ashmit tomar <ashmitt686@gmail.com>
2025-05-27 17:26:14 +0530

    Delete TipCalculator.py

--- TipCalculator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator!")
-bill= float(input("What's your total bill: $"))
-tip= int(input("How much tip would you like to give? 10, 12, or 15: "))
-people= int(input("how many people to split the bill: "))
-percentage= (tip/100)
-calculation= float((bill * percentage + bill) / people)
-print(f"Each person should pay: ${calculation}")
